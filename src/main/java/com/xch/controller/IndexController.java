177b9a50ffd57d0de50ce436b69bea9e438ba2ee
chenghuxie <854575570@qq.com>
2020-04-29 17:46:11 +0800

    基于list手写map jdk  1.8

---
+++ src/main/java/com/xch/controller/IndexController.java
@@ -0,0 +1,11 @@
+package com.xch.controller;
+
+/**
+ * @author xiech
+ * @create 2020-04-29 17:45
+ */
+public class IndexController {
+    public static void main(String[] args) {
+        System.out.println("测试分支！");
+    }
+}
